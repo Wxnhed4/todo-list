fe15c13ffabac101e2a8737def6d85a53584e8a3
shakar <shakarsaman8@gmail.com>
2024-01-01 00:10:59 +0300

    changed the bgc

---
+++ index.js
@@ -0,0 +1,17 @@
+const ul = document.querySelector(".ul");
+const btn = document.querySelector(".btn");
+const inp = document.querySelector(".inp");
+
+btn.addEventListener("click" , () => {
+  let li = document.createElement("li");
+  li.innerHTML = `${inp.value}`;
+  if(inp.value !== ""){
+    ul.appendChild(li);
+    inp.value = "";
+  }else{
+    console.log("kys")
+  }
+  li.addEventListener("dblclick", () => {
+    li.style.textDecoration = "line-through";
+  });
+});
